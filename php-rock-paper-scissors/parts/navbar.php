4fa44e6944c696d50b81a784515459000ae8c24d
関目智史 <zou3road@gmail.com>
2023-06-12 22:02:46 +0900

    Add:ナビバーにユーザー名を表示

--- php-rock-paper-scissors/parts/navbar.php
+++ php-rock-paper-scissors/parts/navbar.php
@@ -1,6 +1,9 @@
 <nav class="navbar navbar-dark bg-primary">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">Rock Paper Scissors</a>
-    <div class="text-white">ようこそ、xxさん！</div>
+    <!-- <div class="text-white">ようこそ、xxさん！</div> -->
+    <?php if (isset($_SESSION['userName'])): ?>
+      <div class="text-white">ようこそ、<?php echo $_SESSION['userName']?>さん</div>
+    <?php endif; ?> 
   </div>
 </nav>
